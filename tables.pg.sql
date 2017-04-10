2888e156c460aec5509385f6e0da83e5ef590500
koolay <koolay@163.com>
2017-04-10 08:23:00 +0800

    modify table

--- tables.pg.sql
+++ tables.pg.sql
@@ -1,5 +1,5 @@
 CREATE TABLE public.slow_sql (
-  created_on TIMESTAMP WITHOUT TIME ZONE,
+  created_on TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
   "user" CHARACTER VARYING(200),
   host CHARACTER VARYING(100),
   query_time DOUBLE PRECISION,
@@ -7,6 +7,7 @@ CREATE TABLE public.slow_sql (
   rows_sent INTEGER,
   rows_examined INTEGER,
   sql TEXT,
-  id INTEGER PRIMARY KEY NOT NULL DEFAULT nextval('slow_sql_id_seq'::regclass)
+  id INTEGER PRIMARY KEY NOT NULL DEFAULT nextval('slow_sql_id_seq'::regclass),
+  "when" TIMESTAMP WITHOUT TIME ZONE
 );
 CREATE UNIQUE INDEX slow_sql_id_uindex ON slow_sql USING BTREE (id);
